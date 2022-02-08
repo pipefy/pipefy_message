bb27eba0dc511aa79b39617da19a102eac94d4e0
Jleber <joao.leber@pipefy.com>
2022-02-08 11:54:45 -0300

    Add initial configuration for aws

--- lib/pipefy_message.rb
+++ lib/pipefy_message.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 require_relative "pipefy_message/version"
+require_relative "pipefy_message/configuration"
 
 module PipefyMessage
   # Simple Test class to validate the project
   class Test
+
     def hello
+      sns = Aws::SNS::Resource.new
+
+      puts sns.topics
       puts "It's Alive !"
     end
   end
---
+++ lib/pipefy_message/configuration.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "aws-sdk-sns"
+
+module PipefyMessage
+  # Aws Provider Config class to connect with the cloud resources
+  class AwsProviderConfig
+    def self.do_connection
+      Aws.config.update(
+        endpoint: ENV["AWS_ENDPOINT"],
+        access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+        secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        region: ENV["AWS_REGION"] || "us-east-1"
+      )
+    end
+  end
+
+end
+
