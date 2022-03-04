2223190b3304bafb5fbd4117fb8ab0b270440ca0
Jleber <joao.leber@pipefy.com>
2022-03-04 15:21:16 -0300

    Adjust project modules

--- lib/pipefy_message/broker/aws/configuration.rb
+++ lib/pipefy_message/broker/aws/configuration.rb
@@ -4,18 +4,22 @@ require "aws-sdk-sns"
 require "singleton"
 
 module PipefyMessage
-  # Aws Provider Config class to connect with the cloud resources
-  class AwsProviderConfig
-    include Singleton
+  module BrokerConfiguration
+    module AwsProvider
+      # Aws Provider Config class to connect with the cloud resources
+      class ProviderConfig
+        include Singleton
 
-    def setup_connection
-      Aws.config.update(
-        endpoint: ENV["AWS_ENDPOINT"],
-        access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-        secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-        region: ENV["AWS_REGION"] || "us-east-1",
-        stub_responses: true
-      )
+        def setup_connection
+          Aws.config.update(
+            endpoint: ENV["AWS_ENDPOINT"],
+            access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+            secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+            region: ENV["AWS_REGION"] || "us-east-1",
+            stub_responses: ENV["AWS_CLI_STUB_RESPONSE"] || false
+          )
+        end
+      end
     end
   end
 end
--- lib/pipefy_message/broker/aws/sns/publisher.rb
+++ lib/pipefy_message/broker/aws/sns/publisher.rb
@@ -2,38 +2,45 @@
 
 require "aws-sdk-sns"
 require "json"
+require_relative "../configuration"
 
 module PipefyMessage
-  # Aws SNS Publisher class to publish json messages into a specific topic
-  class SnsPublisher
-    def initialize
-      PipefyMessage::AwsProviderConfig.instance.setup_connection
-      @sns = Aws::SNS::Resource.new
-    end
+  module Publisher
+    module AwsProvider
+      # Aws SNS Publisher class to publish json messages into a specific topic
+      class SnsPublisher
+        def initialize
+          aws_config = PipefyMessage::BrokerConfiguration::AwsProvider::ProviderConfig.instance
+          aws_config.setup_connection
+          @sns = Aws::SNS::Resource.new
+        end
 
-    def publish(payload, topic_arn)
-      message = prepare_payload(payload)
-      do_publish(message, topic_arn)
-    end
+        def publish(payload, topic_arn)
+          message = prepare_payload(payload)
+          do_publish(message, topic_arn)
+        end
 
-    private
+        private
 
-    def prepare_payload(payload)
-      # The 'Default' json key/entry it's mandatory to ruby sdk
-      {
-        "default" => payload
-      }
-    end
+        def prepare_payload(payload)
+          # The 'Default' json key/entry it's mandatory to ruby sdk
+          {
+            "default" => payload
+          }
+        end
 
-    def do_publish(message, topic_arn)
-      topic = @sns.topic(topic_arn)
+        def do_publish(message, topic_arn)
+          topic = @sns.topic(topic_arn)
 
-      puts "Publishing a json message to topic #{topic_arn}"
-      result = topic.publish({
-                               message: message.to_json,
-                               message_structure: "json"
-                             })
-      puts "Message Published with ID #{result.message_id}"
+          puts "Publishing a json message to topic #{topic_arn}"
+          result = topic.publish({
+                                   message: message.to_json,
+                                   message_structure: "json"
+                                 })
+          puts result
+          puts "Message Published with ID #{result.message_id}"
+        end
+      end
     end
   end
 end
