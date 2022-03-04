2d2e93489c7ee7a9ae809edd0cd9eb533786b8cf
Jleber <joao.leber@pipefy.com>
2022-03-04 15:21:55 -0300

    Adjust tests

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
-RSpec.describe PipefyMessage::AwsProviderConfig do
+RSpec.describe PipefyMessage::BrokerConfiguration::AwsProvider::ProviderConfig do
   context "when I try to connect with AWS provider" do
     it "should return a hash with correct values" do
-      keys = PipefyMessage::AwsProviderConfig.instance.setup_connection
+      keys = PipefyMessage::BrokerConfiguration::AwsProvider::ProviderConfig.instance.setup_connection
 
       expected = { endpoint: ENV["AWS_ENDPOINT"], access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-                   secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], region: "us-east-1", stub_responses: true }
+                   secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], region: "us-east-1",
+                   stub_responses: (ENV["AWS_CLI_STUB_RESPONSE"] || false) }
 
       expect(keys).to eq expected
     end
 
     it "should return a Singleton" do
-      first_instance = PipefyMessage::AwsProviderConfig.instance
-      second_instance = PipefyMessage::AwsProviderConfig.instance
+      first_instance = PipefyMessage::BrokerConfiguration::AwsProvider::ProviderConfig.instance
+      second_instance = PipefyMessage::BrokerConfiguration::AwsProvider::ProviderConfig.instance
 
       expect(first_instance).to eq(second_instance)
     end
