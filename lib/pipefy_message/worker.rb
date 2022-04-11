c6b6bba3453e778c372ee24306c6c90a75f77104
Eduardo Hattori <eduardohattorif@gmail.com>
2022-04-11 17:47:16 -0300

    feat: add self.included to add attr in runtime

--- lib/pipefy_message/worker.rb
+++ lib/pipefy_message/worker.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 module PipefyMessage
+  # ClassMethods
   module Worker
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
     # ClassMethods
-    module ClassMethods #ActiveJob
-      def perform(body, options = {}); end
-      def pipefymessages_options(opts = {}); end
+    module ClassMethods
+      def pipefymessage_options(opts = {})
+        options_hash = PipefyMessage.default_worker_options.merge(opts.transform_keys(&:to_s))
+        options_hash.each do |k, v|
+          singleton_class.class_eval { attr_accessor k }
+          send("#{k}=", v)
+        end
+      end
     end
   end
 end
