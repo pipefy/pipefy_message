df2b7cfe133c6a667de4ad5aabe0f52d5e99844f
Eduardo Hattori <eduardohattorif@gmail.com>
2022-04-11 14:41:21 -0300

    feat: add worker to boot app

--- lib/pipefy_message.rb
+++ lib/pipefy_message.rb
@@ -6,6 +6,7 @@ require_relative "pipefy_message/broker/aws/sns/publisher"
 require_relative "pipefy_message/base_consumer"
 require_relative "pipefy_message/base_publisher"
 require_relative "pipefy_message/logger"
+require_relative "pipefy_message/worker"
 require "logger"
 
 module PipefyMessage
