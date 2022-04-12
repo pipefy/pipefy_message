21b9a526855159896986c0f88e8622f6daa1b732
Eduardo Hattori <eduardohattorif@gmail.com>
2022-04-12 17:57:42 -0300

    feat: implement a perform_async method

--- lib/pipefy_message/worker.rb
+++ lib/pipefy_message/worker.rb
@@ -1,20 +1,37 @@
 # frozen_string_literal: true
 
+require 'singleton'
+
 module PipefyMessage
   # ClassMethods
   module Worker
     def self.included(base)
       base.extend(ClassMethods)
     end
-
+    
     # ClassMethods
-    module ClassMethods
-      def pipefymessage_options(opts = {})
+    module ClassMethods 
+      def pipefymessage_options(opts = {})        
         options_hash = PipefyMessage.default_worker_options.merge(opts.transform_keys(&:to_s))
         options_hash.each do |k, v|
           singleton_class.class_eval { attr_accessor k }
           send("#{k}=", v)
         end
+      end
+      
+      def perform_async()
+        instance_broker = build_instance_broker
+        obj = self.new 
+        instance_broker.poller do |message|
+          obj.perform(message)
+        end 
+      end
+
+      def build_instance_broker()        
+        map = {"aws" => "PipefyMessage::Providers::AwsBroker"}
+        require_relative "providers/#{self.broker}_broker"
+        
+        map[self.broker].constantize.new(self.queue)
       end
     end
   end
--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -2,28 +2,37 @@
 
 RSpec.describe PipefyMessage::Worker do
   $result =~ nil
+
+  # class MockBroker < Broker
+  #   def poller
+  #     yield("test")
+  #   end
+  # end
   
   class TestWorker
     include PipefyMessage::Worker
     pipefymessage_options broker: "aws"
 
-    def perform(body)
-      $result = body
+    def perform(message)
+      $result = message
     end
   end
 
   describe "#perform" do
-    it "allow call .perform" do
-      worker = TestWorker.new
-      worker.perform("test")
+
+    #mock PipefyMessage::Providers::AwsBroker -> MockBroker
+
+    it "allow call .perform from instance worker when call perform_async from ClassMethod" do
+      TestWorker.perform_async
       expect($result).to eq "test"
     end
   end
 
   describe "#options class" do
     it "should set options in class" do
-      TestWorker.new
       expect(TestWorker.broker).to eq "aws"
     end
   end
+
+  
 end
