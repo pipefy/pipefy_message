80a06e20022ce90060e47caf9d5506ae82ef1b90
Eduardo Hattori <eduardohattorif@gmail.com>
2022-04-13 15:59:15 -0300

    feat: add config to broker

--- lib/pipefy_message/providers/aws_broker.rb
+++ lib/pipefy_message/providers/aws_broker.rb
@@ -4,9 +4,32 @@ require "json"
 module PipefyMessage
     module Providers
         class AwsBroker < Broker
+
+            @@default_options = { 
+                access_key_id:  (ENV["AWS_ACCESS_KEY_ID"] || "foo"),
+                secret_access_key: (ENV["AWS_SECRET_ACCESS_KEY"] || "bar"),
+                endpoint:  (ENV["AWS_ENDPOINT"] || "http://localhost:4566"),
+                region:  (ENV["AWS_REGION"] || "us-east-1"),
+                stub_responses: (ENV["AWS_CLI_STUB_RESPONSE"] || "true")
+            }
+
             def initialize(queue_url)
+
+                @config = AwsBroker.config_options
+                
+                Aws.config.update(@config)
+
+                
+                @sqs = Aws::SQS::Client.new(region: @config[:region])                
+                require "pry"; binding.pry
+
                 @poller = Aws::SQS::QueuePoller.new(queue_url)
+                @poller.before_request { stop! if user_interrupt == true }
                 @wait_time_seconds = 10
+            end
+
+            def self.config_options
+                @@default_options
             end
 
             def poller()
