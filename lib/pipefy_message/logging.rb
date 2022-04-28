23cb0319913085c9b9315b898ddd119c646de114
Eduarda Ferreira <eduarda.ferreira@pipefy.com>
2022-04-28 17:30:49 -0300

    refact (post-review): Remove logger methods made unnecessarily verbose by latest changes

--- lib/pipefy_message/logging.rb
+++ lib/pipefy_message/logging.rb
@@ -18,8 +18,8 @@ module PipefyMessage
     ##
     # Creates a logger object if it has not yet been instantiated,
     # or returns the existing object.
-    def self.logger
-      @logger ||= logger_setup
+    def logger
+      @logger ||= Logging.logger_setup
     end
 
     ##
@@ -30,37 +30,16 @@ module PipefyMessage
       level ||= loglevels.index ENV.fetch("ASYNC_LOG_LEVEL", "INFO")
       level ||= Logger::ERROR
       logger.level = level
-      logger
-    end
 
-    ##
-    # Formats logger output as a JSON object, including information on
-    # the calling object. Should not be called directly; this method is
-    # called implicitly whenever a logger method is called.
-    def self.json_output(_obj, severity, datetime, progname, msg)
-      { date: datetime.to_s,
-        level: severity.to_s,
-        app: progname.to_s,
-        context: "async_processing",
-        message: msg }
-    end
-
-    ##
-    # Logger method available to all instances of classes
-    # that include the Logging module (as an instance method).
-    # Includes information on the calling object.
-    def logger
-      Logging.logger.formatter = proc do |severity, datetime, progname, msg|
-        json_hash = Logging.json_output(self, severity, datetime, progname, msg)
-
-        # The necessity of explicitly JSON.dumping this hash has been
-        # discussed in a code review. We should check how these logs
-        # will be processed on the other end and perhaps refactor
-        # accordingly.
-        JSON.dump(json_hash) + ($INPUT_RECORD_SEPARATOR || "\n")
+      logger.formatter = proc do |severity, datetime, progname, msg|
+        { date: datetime.to_s,
+          level: severity.to_s,
+          app: progname.to_s,
+          context: "async_processing",
+          message: msg }.to_json + $INPUT_RECORD_SEPARATOR.to_s
       end
 
-      Logging.logger
+      logger
     end
 
     ##
