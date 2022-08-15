95a4f03683c858a0f7d4d1c7a5de8a75e6285d57
Eduarda Ferreira <eduarda.ferreira@pipefy.com>
2022-08-15 17:39:16 -0300

    feat/refact: Create QueueDoesNotExist error and remove separate logging on exception init
    (That is a particularly common error atm)

--- lib/pipefy_message/providers/errors.rb
+++ lib/pipefy_message/providers/errors.rb
@@ -8,53 +8,23 @@ module PipefyMessage
     # as common error messages.
     module Errors
       ##
-      # Enables automatic error logging when prepended to a custom error
-      # class.
-      #
-      # In order for this module to work, the prepending class must
-      # inherit from the Ruby Exception class. Note that this condition
-      # is satisfied by any custom error class that inherits from
-      # StandardError and its children, such as RuntimeError, given that
-      # StandardError is itself a child of Exception.
-      #
-      # The reason to use prepend rather than include is to make this
-      # module come below the error class in the inheritance chain.
-      # This ensures that, when an error is raised and its constructor
-      # is called, this module's initialize method gets called first,
-      # then calls the original constructor with super, and calls the
-      # logger once initialization is done. This effectively "wraps"
-      # the error initialize method in the logging constructor below.
-      module LoggingError
-        prepend PipefyMessage::Logging
+      # Abstraction for service and networking errors.
+      class ResourceError < RuntimeError
+      end
 
-        def initialize(msg = nil)
+      ##
+      # Abstraction for errors caused by nonexisting queues, such as
+      # Aws::SQS::Errors::QueueDoesNotExist.
+      class QueueDoesNotExist < ResourceError
+        def initialize(msg = "The specified queue does not exist")
           super
-          logger.error({
-                         error_class: self.class,
-                         error_message: message,
-                         stack_trace: full_message
-                       })
-
-          # message and full_message are methods provided by the
-          # Ruby Exception class.
-          # The hash keys used above were an attempt to provide more
-          # descriptive names than those of the original methods (:P),
-          # but this has still led to some confusion, so, to be more
-          # explicit: if e is an instance of Exception (or its
-          # children), e.message returns the error message for e and
-          # e.full_message provides the full stack trace. This is
-          # what's being included in the logs above. Logging inside
-          # the constructor ensures information is logged as soon
-          # as the error is raised.
-          # For details, please refer to the official documentation for
-          # the Exception class.
         end
       end
 
       ##
-      # Abstraction for service and networking errors.
-      class ResourceError < RuntimeError
-        prepend PipefyMessage::Providers::Errors::LoggingError
+      # Abstraction for provider authorization errors, such as
+      # Aws::SNS::Errors::AuthorizationError.
+      class AuthorizationError < RuntimeError
       end
 
       ##
@@ -62,7 +32,6 @@ module PipefyMessage
       # to a method call (eg: if a queueing service client is
       # initialized with an invalid queue identifier).
       class InvalidOption < ArgumentError
-        prepend PipefyMessage::Providers::Errors::LoggingError
       end
 
       # Error messages:
